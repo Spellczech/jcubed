6732db7519b5b28c29e00a24a15628828300856e
Jason Muller <jmull010@plattsburgh.edu>
2018-10-24 23:47:49 -0500

    PLEASE GO THROUG!!!!

--- app/src/main/java/com/example/campusways/campusways/Profile.java
+++ app/src/main/java/com/example/campusways/campusways/Profile.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.profile);
     }
 }
